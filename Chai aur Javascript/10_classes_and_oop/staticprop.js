3b7a494e738ba031217de77cc5a56b9dd6446e91
Dhiraj Udhani <dhirajudhani870@gmail.com>
2024-01-18 08:21:09 +0530

    oops and bind

---
+++ Chai aur Javascript/10_classes_and_oop/staticprop.js
@@ -0,0 +1,26 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username is ${this.username}`);
+    }
+
+    static createId(){ 
+        return `123`
+    }
+}
+
+const dhiraj = new User("dhiraj")
+// console.log(dhiraj.createId())
+
+class Teacher extends User{
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone","i@iphone.com")
+console.log(iphone.createId())
